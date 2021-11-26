9f4e4843a005153eb6f1c03ff7816666bfc501b4
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2021-11-26 09:46:16 +0530

    Mastered bare basics of xstate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Machine } from 'xstate'
+import { useMachine } from '@xstate/react'
 
-function App() {
+const machineExample = Machine({
+  initial:"off",
+  states:{
+    off:{
+      on:{
+        ACTIVATE:"activateProcess"
+      }
+    },
+    activateProcess:{
+      on:{
+        DEACTIVATE:"deactivateProcess"
+      }
+    },
+    deactivateProcess:{
+      on:{
+        TURNOFF:"off"
+      }
+    }
+  }
+})
+
+export default function App(){
+  const [machine,send] = useMachine(machineExample);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  <p>the currently the machine is in the {machine.value} state</p>
+  <button onClick ={() => send('ACTIVATE')} >ACTIVATE PROCESS</button>
+  <button onClick = {() => send("DEACTIVATE")}>DEACTIVATE PROCESS</button>
+  <button onClick = {() => send("TURNOFF")}>TURN OFF PROCESS</button>
+  </>
+  )
 }
 
-export default App;
